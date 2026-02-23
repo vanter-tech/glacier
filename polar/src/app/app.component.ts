be16a85f5a8d6a9c4bdd3f67e4e1cdc2735b1307
Snowsita <Snowsita@users.noreply.github.com>
2026-02-22 23:34:13 -0600

    feat(app): Implement account fetching on initialization

--- polar/src/app/app.component.ts
+++ polar/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {database} from "../../wailsjs/go/models";
+import {GetAccounts} from "../../wailsjs/go/main/App";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'polar';
+
+  accounts: database.Account[] = [];
+
+  ngOnInit(): void {
+    this.fetchAccounts();
+  }
+
+  fetchAccounts(): void {
+    GetAccounts()
+      .then((result: database.Account[]) => {
+        console.log("Accounts from SQLite:", result);
+        this.accounts = result;
+      })
+      .catch((error) => {
+      console.error("Failed to fetch accounts from Go:", error);
+    });
+  }
 }
