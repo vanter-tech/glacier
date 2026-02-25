b06561982c1b9ddc7cef4a3fe8830eab36611f50
Snowsita <Snowsita@users.noreply.github.com>
2026-02-24 21:12:23 -0600

    feat(app): Add runtime functions for profile activation and account retrieval

---
+++ polar/wailsjs/go/models.ts
@@ -0,0 +1,60 @@
+export namespace queries {
+	
+	export class Account {
+	    id: number;
+	    name: string;
+	    type: string;
+	    balance_cents: sql.NullInt64;
+	
+	    static createFrom(source: any = {}) {
+	        return new Account(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.name = source["name"];
+	        this.type = source["type"];
+	        this.balance_cents = this.convertValues(source["balance_cents"], sql.NullInt64);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
+export namespace sql {
+	
+	export class NullInt64 {
+	    Int64: number;
+	    Valid: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new NullInt64(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Int64 = source["Int64"];
+	        this.Valid = source["Valid"];
+	    }
+	}
+
+}
+
