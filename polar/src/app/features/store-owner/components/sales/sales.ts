ef75ca704242d4c8f0d86023e02157455486b0b4
Snowsita <Snowsita@users.noreply.github.com>
2026-02-28 07:17:48 -0600

    feat(store-dashboard): Initialized sales

---
+++ polar/src/app/features/store-owner/components/sales/sales.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales',
+  imports: [],
+  templateUrl: './sales.html',
+  styleUrl: './sales.scss',
+})
+export class Sales {
+
+}
