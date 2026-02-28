5dce6f4f08d511b50ce7026d7ef960f64857878c
Snowsita <Snowsita@users.noreply.github.com>
2026-02-28 07:17:30 -0600

    feat(store-dashboard): Initialized clients

---
+++ polar/src/app/features/store-owner/components/clients/clients.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients',
+  imports: [],
+  templateUrl: './clients.html',
+  styleUrl: './clients.scss',
+})
+export class Clients {
+
+}
