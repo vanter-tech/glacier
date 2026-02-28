eda28fa735a8ea6efff0ba0f4cab44ce2d475100
Snowsita <Snowsita@users.noreply.github.com>
2026-02-28 06:43:33 -0600

    feat(profile): Refactor profile management and introduce Profile struct

--- iceberg/database/profile.go
+++ iceberg/database/profile.go
@@ -1,43 +1,17 @@
 package database
 
-import (
-	"context"
-	"fmt"
-	"glacier/iceberg/database/queries"
-	"log"
-)
+type Profile struct {
+	FirstName string
+	LastName  string
+	ProfileId int
+}
 
-func ActivateProfile(profileType string) error {
-	err := Q.SetAppSetting(context.Background(), queries.SetAppSettingParams{
-		Key:   "active_profile",
-		Value: profileType,
-	})
-	if err != nil {
-		return fmt.Errorf("could not set active profile: %v", err)
-	}
+func NewProfile() *Profile {
+	return &Profile{}
+}
 
-	if profileType == "personal" {
-		err = CreatePersonalSchema()
-	} else if profileType == "store" {
-		err = CreateStoreSchema()
-	} else {
-		return fmt.Errorf("invalid profile type: %s", profileType)
-	}
-
-	if err != nil {
-		return fmt.Errorf("failed to initialize %s profile: %v", profileType, err)
-	}
-
-	settingKey := fmt.Sprintf("has_%s_profile", profileType)
-
-	err = Q.SetAppSetting(context.Background(), queries.SetAppSettingParams{
-		Key:   settingKey,
-		Value: "true",
-	})
-
-	if err != nil {
-		log.Println("Could not save profile creation status:", err)
-	}
+func (p *Profile) ActivateProfile(profileType string) error {
+	InitDB()
 
 	return nil
 }
