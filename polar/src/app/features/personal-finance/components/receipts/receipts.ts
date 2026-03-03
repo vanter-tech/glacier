b766cc39bc9e056a022ef40af749ce513235fd23
Snowsita <Snowsita@users.noreply.github.com>
2026-03-02 20:34:13 -0600

    feat(receipts): enhance receipt management with modal and translation support

--- polar/src/app/features/personal-finance/components/receipts/receipts.ts
+++ polar/src/app/features/personal-finance/components/receipts/receipts.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+import {Modal} from '../../../../shared/modal/modal';
+import {ReceiptForm} from './components/receipt-form/receipt-form';
 
 @Component({
   selector: 'app-receipts',
-  imports: [],
+  imports: [
+    TranslatePipe,
+    Modal,
+    ReceiptForm
+  ],
   templateUrl: './receipts.html',
   styleUrl: './receipts.scss',
 })
 export class Receipts {
+  readonly iconPath = '/favicon.ico';
 
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
