eb49a2b581e9ac34e2c7661e2db56b6ecc83fe6d
Snowsita <Snowsita@users.noreply.github.com>
2026-03-04 07:10:57 -0600

    feat(receipts): update modal handling and implement receipt saving functionality

--- polar/src/app/features/personal-finance/components/receipts/receipts.ts
+++ polar/src/app/features/personal-finance/components/receipts/receipts.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
 import {Modal} from '../../../../shared/modal/modal';
 import {ReceiptForm} from './components/receipt-form/receipt-form';
+import {CreateReceipt} from '../../../../../../wailsjs/go/main/App';
 
 @Component({
   selector: 'app-receipts',
@@ -16,13 +17,26 @@ import {ReceiptForm} from './components/receipt-form/receipt-form';
 export class Receipts {
   readonly iconPath = '/favicon.ico';
 
-  isModalOpen = false;
+  isModalOpen = signal(false);
 
   openModal() {
-    this.isModalOpen = true;
+    this.isModalOpen.set(true);
   }
 
   closeModal() {
-    this.isModalOpen = false;
+    this.isModalOpen.set(false);
+  }
+
+  async saveReceipt(formData: any) {
+    try {
+      const newReceipt = await CreateReceipt(formData.amount, formData.date, formData.description || '');
+
+      console.log("Succesfully saved to SQLite:", newReceipt);
+
+      this.closeModal();
+      console.log(this.isModalOpen)
+    } catch (error) {
+      console.error("Failed to save receipt:", error);
+    }
   }
 }
